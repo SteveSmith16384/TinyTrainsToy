3c4f59c9e8b0a8998bb6caa83e91286ead62a93c
= <stephen.carlylesmith@googlemail.com>
2022-10-09 14:39:29 +0100

    Added Tileset

--- src/Train.gd
+++ src/Train.gd
@@ -1,16 +1,10 @@
 extends PathFollow2D
 
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	
+	pass
 
 
 func _process(delta):
-	offset += delta * 10
+	offset += delta * 20
 	pass
--- src/TrainPath.gd
+++ src/TrainPath.gd
@@ -5,4 +5,7 @@ func _ready():
 	curve.add_point(Vector2(20, 20))
 	curve.add_point(Vector2(50, 30))
 	curve.add_point(Vector2(70, 90))
+	curve.add_point(Vector2(20, 20))
+	
+	$TileMap.set_cell(0, 0, 0)
 	pass
